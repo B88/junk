cc496e0a17c4e4d84f6faeba2b84a4b10f358ee6
Bryce Baker <baker.b.rice@gmail.com>
2017-10-26 10:46:26 -0700

    test2

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CSU East Bay' });
 });
 
 router.get('/doit', function(req, res, next) {
     res.render('doit', { title: 'Do it' });
-});
-
-router.get('/doit2', function(req, res, next) {
-    res.render('doit2', { title: 'Do it2' });
 });
 
 module.exports = router;
